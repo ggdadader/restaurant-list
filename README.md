8408683af6cefaca597ef5d2ea77d6eafee1831e
ggdadader <xider1091@gmail.com>
2021-10-17 04:30:16 -0400

    finish the search function, show pages, readme, as final version.

---
+++ README.md
@@ -0,0 +1,30 @@
+# Restaurant List
+
+ Web App exercise: implement a simple website server(localhost) with Express.js.
+
+Website theme: restaurant review site.
+
+## Main Function
+
+1. Users can browse all restaurants' basic info and comments.
+2. Users can click each restaurants for more info.
+3. Users can filter out the restaurants by keywords of name or categories.
+
+</br>
+## Installation and Execution
+
+Clone the project
+```
+git clone https://ggdadader.github.io/restaurant-list.git
+```
+Run the following code on your command line
+```
+npm install
+```
+```
+npm run dev
+```
+Open your browser and type in the following address
+```
+http://localhost:3000/
+```
--- app.js
+++ app.js
@@ -5,15 +5,44 @@ const port = 3000
 
 // require express-handlebars here
 const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+// setting static files
+app.use(express.static('public'))
+
 
 // routes setting
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index',{restaurants: restaurantList.results})
 })
+app.get('/restaurants/:id', (req, res) => {
+  const selectedRestaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.id)
+  res.render('show', { restaurant: selectedRestaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const keywordList = keyword.split(' ').filter(item => item !== '')
+
+  if (keywordList.length <= 0) {
+    return res.redirect('/')
+  }
+  const restaurants = restaurantList.results.filter(item => {
+   
+    for (let i = 0; i < keywordList.length; i++) {
+      if (item.name.includes(keywordList[i]) || item.category.includes(keywordList[i])) {
+        return true
+      }
+    }
+  })
+  res.render('search', { keyword, restaurants })
+})
+  
+
+
 
 // start and listen on the Express server
 app.listen(port, () => {
